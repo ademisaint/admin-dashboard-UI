59ef17ffefff22c161ffe46027705d3a15311193
ademisaint <saintademilayo@gmail.com>
2024-04-21 23:57:50 +0100

    dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React from 'react';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Line } from 'react-chartjs-2';
 import 'chart.js/auto';
 import Piechart from '../component/Piechart';
 import ComplexTable from '../component/ComplexTable';
 import CheckTable from '../component/CheckTable';
 import Calender from '../component/Calender';
+import fire from '../assets/images/local_fire_department.png';
+import timer from '../assets/images/timer.png';
+import slowMotionVideo from'../assets/images/slow_motion_video.png'
+import avatar1 from '../assets/images/Avatar 1.png'
+import avatar2 from '../assets/images/Avatar 2.png'
+import avatar3 from '../assets/images/Avatar 3.png'
+import avatar4 from '../assets/images/Avatar 4.png'
+import teamM1 from '../assets/images/teamM1.png'
+import teamM2 from '../assets/images/teamM2.png'
+import teamM3 from '../assets/images/teamM3.png'
 
-import { ResponsiveBar } from '@nivo/bar'
+
+import { ResponsiveBar } from '@nivo/bar';
 
 const Dashboard = () => {
     
@@ -451,6 +464,94 @@ const Dashboard = () => {
 
         </div>
 
+        <div className='flex justify-between my-[20px]'>
+            <div className='w-[50%] mr-[10px] flex'>
+                <div className='w-[50%] mr-[5px] bg-white rounded-[20px] p-[20px]'>
+                    <div className="lesson-header flex items-center pb-[20px] pt-[5px]">
+                        <div className="lesson-header-icon p-[10px] bg-[#FEEFEE] rounded-[15px]">
+                            <img src={fire} alt="fire-image" />
+                        </div>
+                        <div className='pl-[10px]'>
+                            <p className='text-[#A3AED0] text-[12px] font-bold'>Business Design</p>
+                            <h1 className='text-[#2B3674] text-[16px] font-bold'>New lesson is available</h1>
+                        </div>
+                    </div>
+                    <div className="lesson-title font-bold text-[20px] text-[#2B3674] pt-[20px]">
+                        What do you need to know to create better products?
+                    </div>
+                    <div className="lesson-info flex pt-[30px]">
+                        <div className='flex items-center pr-[12px]'>
+                            <div className="lesson-info-icon">
+                                <img src={timer} alt="timer-icon" />
+                            </div>
+                            <div className='text-[14px] text-[#2B3674] font-bold px-[5px]'>85 mins</div>
+                        </div>
+                        <div className='flex items-center pl-[12px]'>
+                            <div className="lesson-info-icon">
+                                <img src={slowMotionVideo} alt="motion-vi" />
+                            </div>
+                            <div className='text-[14px] text-[#2B3674] font-bold px-[5px]'>Video format</div>
+                        </div>
+                    </div>
+                    <div className="lesson-footer flex items-center justify-between pt-7 pb-2">
+                        <div className="lesson-age-restriction flex">
+                            <div className='flex'>
+                                <div className=''><img src={avatar1} alt="avatar-1" width={34} height={34}/></div>
+                                <div className='ml-[-15px]'><img src={avatar2} alt="avatar-2" /></div>
+                                <div className='ml-[-15px]'><img src={avatar3} alt="avatar-3" /></div>
+                                <div className='ml-[-15px]'><img src={avatar4} alt="avatar-4" /></div>
+                                <div className='ml-[-15px] border-white border-[3px] p-[5] bg-[#E0E5F2] rounded-full flex items-center w-[34px] h-[34px] text-[#4318FF] font-bold text-[9px] justify-center'>18+</div>
+                            </div>
+                            
+                        </div>
+                        <button className="get-started-btn text-[white] text-[14px] font-bold px-[20px] py-[10px] bg-[#4318FF] rounded-[16px]">Get Started</button>
+                    </div>
+                </div>
+
+                <div className='w-[50%] mr-[5px] bg-white rounded-[20px] p-[20px]'>
+                    <div className='flex justify-between items-center pb-[10px] pt-[5px]'>
+                        <h1 className='text-[#2B3674] text-[16px] font-bold'>Team members</h1>
+                        <div className='bg-[#F4F7FE] text-[#4318FF] w-[37px] h-[37px] rounded-[10px] flex items-center justify-center'><AddCircleIcon/></div>
+                    </div>
+                    <div>
+                        <div className='flex justify-between items-center'>
+                            <div className='flex'>
+                                <img src={teamM1} alt="avatar1" />
+                                <div>
+                                    <h1 className='font-bold text-[16px] text-[#2B3674]'>Adela Parkson</h1>
+                                    <p className='text-[#A3AED0] text-[12px] font-medium'>Creative Director</p>
+                                </div>
+                            </div>
+                            <div className='text-[#A3AED0]'><MoreVertIcon/></div>
+                        </div>
+                        <div className='flex justify-between items-center'>
+                            <div className='flex'>
+                                <img src={teamM2} alt="avatar2" />
+                                <div>
+                                    <h1 className='font-bold text-[16px] text-[#2B3674]'>Christian Mad</h1>
+                                    <p className='text-[#A3AED0] text-[12px] font-medium'>Product Designer</p>
+                                </div>
+                            </div>
+                            <div className='text-[#A3AED0]'><MoreVertIcon/></div>
+                        </div>
+                        <div className='flex justify-between'>
+                            <div className='flex'>
+                                <img src={teamM3} alt="avatar3" />
+                                <div>
+                                    <h1 className='font-bold text-[16px] text-[#2B3674]'>Jason Statham</h1>
+                                    <p className='text-[#A3AED0] text-[12px] font-medium'>Junior Graphic Designer</p>
+                                </div>
+                            </div>
+                            <div className='text-[#A3AED0]'><MoreVertIcon/></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='w-[50%] mr-[10px] flex'>
+                
+            </div>
+        </div>
     </div>
   )
 }
